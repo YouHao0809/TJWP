edd0f1e77528cfc753fa4434377a6325ebaa05b9
YouHao0809 <youhao0809@outloook.com>
2021-08-22 22:56:37 +0800

    Update index.js

--- cloudfunctions/mylogin/index.js
+++ cloudfunctions/mylogin/index.js
@@ -1,4 +1,4 @@
-
+const cloud = require('wx-server-sdk')
 const Token = require('Token')
 
 // 云函数入口函数
@@ -6,12 +6,30 @@ exports.main = async (event, context) => {
   account = event.account
   password = event.password
   
-  if(account == "001" && password =="1750000"){
+  const db = cloud.database()
+  db.collection('personal_info').doc('todo-identifiant-aleatoire').get({
+    success: function(res) {
+      // res.data 包含该记录的数据
+      console.log(res.data)
+    }
+  })
+var myres
+  db.collection('personal_info').where({
+    _id: account
+  }).get({
+    success: function(res) {
+      myres = res
+    console.log(res)
+   }
+  })
+
+  if(true){
     return {
       status:"success",
       token:Token.encrypt({account:account},'5h'),
       account:account,
       password:password,
+      res: myres
     }
   }
   else {
